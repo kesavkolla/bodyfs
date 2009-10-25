1948af28c8f575c40efa0689f6ef2b13b97da47a
kesavkolla <kesavkolla@1fa41b22-b2bc-11de-a03c-433460d76767>
2009-10-25 06:24:17 +0000

    Added main page and calendar

---
+++ body-fs/src/com/bodyfs/ui/MainWindowComposer.java
@@ -0,0 +1,33 @@
+package com.bodyfs.ui;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.ForwardEvent;
+import org.zkoss.zk.ui.util.GenericAutowireComposer;
+import org.zkoss.zul.Include;
+
+public class MainWindowComposer extends GenericAutowireComposer {
+
+	private static final long serialVersionUID = 7255967269134815134L;
+	private static Log LOGGER = LogFactory.getLog(MainWindowComposer.class);
+
+	private Include xcontents;
+
+	@Override
+	public void doAfterCompose(final Component comp) throws Exception {
+		LOGGER.fatal("Entering after compose");
+		super.doAfterCompose(comp);
+	}
+
+	public void onHandleClick(ForwardEvent event) {
+		final String id = event.getOrigin().getTarget().getId();
+		LOGGER.debug("Event id: " + id);
+		if (id.equals("btnCustomers")) {
+
+		} else if (id.equals("btnScheduling")) {
+			xcontents.setSrc("/WEB-INF/views/calendar.zul");
+			return;
+		}
+	}
+}

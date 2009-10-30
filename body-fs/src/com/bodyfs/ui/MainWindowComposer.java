14aa4218a119dadcb67de02c9fd8984b1d2326f4
kesavkolla <kesavkolla@1fa41b22-b2bc-11de-a03c-433460d76767>
2009-10-30 23:29:56 +0000

    Added support for OpenId authentication.  This is now using dyuproject and jquery openid selector.

--- body-fs/src/com/bodyfs/ui/MainWindowComposer.java
+++ body-fs/src/com/bodyfs/ui/MainWindowComposer.java
@@ -1,11 +1,16 @@
 package com.bodyfs.ui;
 
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.ForwardEvent;
 import org.zkoss.zk.ui.util.GenericAutowireComposer;
 import org.zkoss.zul.Include;
+import org.zkoss.zul.Label;
+
+import com.dyuproject.openid.OpenIdUser;
 
 public class MainWindowComposer extends GenericAutowireComposer {
 
@@ -13,16 +18,24 @@ public class MainWindowComposer extends GenericAutowireComposer {
 	private static Log LOGGER = LogFactory.getLog(MainWindowComposer.class);
 
 	private Include xcontents;
+	private Label username;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void doAfterCompose(final Component comp) throws Exception {
-		LOGGER.fatal("Entering after compose");
 		super.doAfterCompose(comp);
+		final OpenIdUser user = (OpenIdUser) this.requestScope.get(OpenIdUser.ATTR_NAME);
+		LOGGER.error("A: " + user.getA());
+		LOGGER.error("Attributes: " + user.getAttributes());
+		LOGGER.error("Extensions:" + user.getExtensions());
+		final Map<String, String> userinfo = (Map<String, String>) user.getAttribute("info");
+		if (userinfo != null) {
+			username.setValue(userinfo.get("lastname") + ", " + userinfo.get("firstname"));
+		}
 	}
 
 	public void onHandleClick(ForwardEvent event) {
 		final String id = event.getOrigin().getTarget().getId();
-		LOGGER.error("Event id: " + id);
 		if (id.equals("btnCustomers")) {
 			return;
 		} else if (id.equals("btnScheduling")) {

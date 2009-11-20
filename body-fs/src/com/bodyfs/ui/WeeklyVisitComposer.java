520c6ae0f14fab05e0fb99672761164c52829477
kesavkolla <kesavkolla@1fa41b22-b2bc-11de-a03c-433460d76767>
2009-11-20 14:31:25 +0000

    Fixed the NPE in persondao

--- body-fs/src/com/bodyfs/ui/WeeklyVisitComposer.java
+++ body-fs/src/com/bodyfs/ui/WeeklyVisitComposer.java
@@ -10,6 +10,7 @@ import org.zkoss.zkplus.spring.SpringUtil;
 
 import com.bodyfs.dao.IPersonDAO;
 import com.bodyfs.model.PatientVisit;
+import com.bodyfs.model.Person;
 
 /**
  * 
@@ -20,20 +21,20 @@ public class WeeklyVisitComposer extends GenericForwardComposer {
 
 	private static final long serialVersionUID = 3816734829122660780L;
 	private PatientVisit patientVisit = new PatientVisit();
-	private IPersonDAO personDAO = (IPersonDAO) SpringUtil.getBean("personDAO");
+	private IPersonDAO personDAO;
+	private Person person;
 
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
+		personDAO = (IPersonDAO) SpringUtil.getBean("personDAO");
+		person = personDAO.getByEmail("kesavkolla+bodyfs@gmail.com");
 		this.page.setAttribute("patvisit", patientVisit);
-		for (final PatientVisit visit : this.personDAO.GetPatientVisits(1L)) {
-			System.out.println(visit.getVisitDate());
-		}
 	}
 
 	public void onSave(final ForwardEvent event) {
 		if (patientVisit != null) {
-			patientVisit.setPersonId(1L);
+			patientVisit.setPersonId(person != null ? person.getId() : 1L);
 			personDAO.createPatientVisit(patientVisit);
 			System.out.println(patientVisit.getId());
 		}

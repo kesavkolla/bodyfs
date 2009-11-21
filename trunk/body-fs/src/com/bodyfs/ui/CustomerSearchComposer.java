91afe976bae6034f2a5da9a02bddbbc632cbf99c
kesavkolla <kesavkolla@1fa41b22-b2bc-11de-a03c-433460d76767>
2009-11-21 13:20:20 +0000

    clear up the eclipse warnings

--- trunk/body-fs/src/com/bodyfs/ui/CustomerSearchComposer.java
+++ trunk/body-fs/src/com/bodyfs/ui/CustomerSearchComposer.java
@@ -16,21 +16,12 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zkplus.databind.AnnotateDataBinder;
-import org.zkoss.zkplus.spring.SpringUtil;
-import org.zkoss.zul.A;
-import org.zkoss.zul.Div;
-import org.zkoss.zul.ListModel;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
-import org.zkoss.zul.RowRenderer;
 import org.zkoss.zul.api.Button;
-import org.zkoss.zul.api.Grid;
 import org.zkoss.zul.api.Textbox;
-import org.zkoss.zul.api.Window;
 
 import com.bodyfs.PMF;
-import com.bodyfs.dao.IPageDAO;
-import com.bodyfs.dao.IPersonDAO;
 import com.bodyfs.model.Person;
 
 public class CustomerSearchComposer extends GenericForwardComposer {
@@ -39,25 +30,21 @@ public class CustomerSearchComposer extends GenericForwardComposer {
 	 * 
 	 */
 	private static final long serialVersionUID = -7039984932259770671L;
-	
+
 	private static Log LOGGER = LogFactory.getLog(MainWindowComposer.class);
-	
-	private IPersonDAO personDAO = (IPersonDAO) SpringUtil.getBean("personDAO"); 
-	
+
 	AnnotateDataBinder binder;
-	
+
 	Textbox smrtTextbox;
-	
+
 	Button smrtSearchbtn;
-	
+
 	Listbox persons;
-	
-	
-	
+
 	SearchOptions person = new SearchOptions();
-	
-	public Collection<Person> resultSet= new ArrayList<Person>();
-	
+
+	public Collection<Person> resultSet = new ArrayList<Person>();
+
 	public Collection<Person> getResultSet() {
 		return resultSet;
 	}
@@ -66,196 +53,217 @@ public class CustomerSearchComposer extends GenericForwardComposer {
 		this.resultSet = resultSet;
 	}
 
-	
 	/**
 	 * 
 	 */
+	@SuppressWarnings("unchecked")
 	public void doAfterCompose(final Component comp) throws Exception {
 		super.doAfterCompose(comp);
 		comp.setAttribute(comp.getId() + "Ctrl", this, true);
 		binder = new AnnotateDataBinder(comp);
 		PersistenceManager pm = PMF.get().getPersistenceManager();
-		
-		try{
+
+		try {
 			Query query = pm.newQuery(Person.class);
 			resultSet = (Collection<Person>) query.execute();
 			resultSet = pm.detachCopyAll(resultSet);
-			//extendedSearch.setVisible(false);
-		}finally{
+			// extendedSearch.setVisible(false);
+		} finally {
 			pm.close();
 		}
 		binder.loadAll();
 	}
-	
-	
-	public void searchSmart(){
+
+	public void searchSmart() {
 		CompassSearchSession compassSession = PMF.getCompass().openSearchSession();
-		//PersistenceManager pm = PMF.get().getPersistenceManager();
+		// PersistenceManager pm = PMF.get().getPersistenceManager();
 		smrtTextbox.getValue();
-		CompassHits hits =  compassSession.find(smrtTextbox.getValue());
+		CompassHits hits = compassSession.find(smrtTextbox.getValue());
 		setResultSet(null);
-		for(int i=0; i<hits.length();i++){
-			resultSet.add((Person)hits.data(i));
+		for (int i = 0; i < hits.length(); i++) {
+			resultSet.add((Person) hits.data(i));
 		}
-		//resultSet = pm.detachCopyAll(resultSet);
-		LOGGER.error("Amit- "+resultSet.size());
+		// resultSet = pm.detachCopyAll(resultSet);
+		LOGGER.error("Amit- " + resultSet.size());
 		persons.setModel(new ListModelList(resultSet));
 		binder.loadComponent(persons);
-		
+
 	}
-	
-	//public void
-	
-	public void extendedSearch(SearchOptions searchOptions){
-		
+
+	// public void
+
+	public void extendedSearch(SearchOptions searchOptions) {
+
 		StringBuffer qry = new StringBuffer("SELECT USER_DETAIL.* FROM USER_DETAIL, USER_ROLE");
 		qry.append(" WHERE USER_DETAIL.ROLE_ID = USER_ROLE.ROLE_ID");
 
-		if(searchOptions.getFirstName() != null && searchOptions.getFirstName().length() > 0){
+		if (searchOptions.getFirstName() != null && searchOptions.getFirstName().length() > 0) {
 			qry.append(" AND FIRST_NAME LIKE '").append(searchOptions.getFirstName()).append("%'");
 		}
-		if(searchOptions.getLastName() != null && searchOptions.getLastName().length() > 0){
+		if (searchOptions.getLastName() != null && searchOptions.getLastName().length() > 0) {
 			qry.append(" AND LAST_NAME LIKE '").append(searchOptions.getLastName()).append("%'");
 		}
-		if(searchOptions.getEmail() != null && searchOptions.getEmail().length() > 0){
+		if (searchOptions.getEmail() != null && searchOptions.getEmail().length() > 0) {
 			qry.append(" AND EMAIL_ADDRESS LIKE '").append(searchOptions.getEmail()).append("%'");
 		}
-		if(searchOptions.getZip() != null && searchOptions.getZip().length() > 0){
+		if (searchOptions.getZip() != null && searchOptions.getZip().length() > 0) {
 			qry.append(" AND COURSES LIKE '%").append(searchOptions.getZip()).append("%'");
 		}
-		if(searchOptions.getCustomerType() != null){
+		if (searchOptions.getCustomerType() != null) {
 			qry.append(" AND DATE_OF_JOINING >=? ");
 		}
-		if(searchOptions.getTrmtDateFrom() != null && searchOptions.getTrmtDateTo()!= null){
+		if (searchOptions.getTrmtDateFrom() != null && searchOptions.getTrmtDateTo() != null) {
 			qry.append(" AND DATE_OF_START >=? ");
 		}
-		
-		
+
 	}
-	
-	public void onClick$smrtSearchbtn() throws Exception{
-		//searchSmart();
-		
+
+	public void onClick$smrtSearchbtn() throws Exception {
+		// searchSmart();
+
 	}
-	public void onOK$smrtTextbox(Event evt) throws Exception{
+
+	public void onOK$smrtTextbox(Event evt) throws Exception {
 		searchSmart();
 	}
-	
-	public class SearchOptions implements Serializable{
+
+	public class SearchOptions implements Serializable {
 		/**
 		 * 
 		 */
 		private static final long serialVersionUID = 7148090100102182159L;
 		String firstName;
-		String lastName; 
+		String lastName;
 		String customerType;
 		String zip;
-		
+
 		String email;
-		
+
 		String ageGroup;
-		
+
 		Date trmtDateFrom;
 		Date trmtDateTo;
-		
+
 		/**
 		 * @return the firstName
 		 */
 		public String getFirstName() {
 			return firstName;
 		}
+
 		/**
-		 * @param firstName the firstName to set
+		 * @param firstName
+		 *            the firstName to set
 		 */
 		public void setFirstName(String firstName) {
 			this.firstName = firstName;
 		}
+
 		/**
 		 * @return the lastName
 		 */
 		public String getLastName() {
 			return lastName;
 		}
+
 		/**
-		 * @param lastName the lastName to set
+		 * @param lastName
+		 *            the lastName to set
 		 */
 		public void setLastName(String lastName) {
 			this.lastName = lastName;
 		}
+
 		/**
 		 * @return the customerType
 		 */
 		public String getCustomerType() {
 			return customerType;
 		}
+
 		/**
-		 * @param customerType the customerType to set
+		 * @param customerType
+		 *            the customerType to set
 		 */
 		public void setCustomerType(String customerType) {
 			this.customerType = customerType;
 		}
+
 		/**
 		 * @return the zip
 		 */
 		public String getZip() {
 			return zip;
 		}
+
 		/**
-		 * @param zip the zip to set
+		 * @param zip
+		 *            the zip to set
 		 */
 		public void setZip(String zip) {
 			this.zip = zip;
 		}
+
 		/**
 		 * @return the email
 		 */
 		public String getEmail() {
 			return email;
 		}
+
 		/**
-		 * @param email the email to set
+		 * @param email
+		 *            the email to set
 		 */
 		public void setEmail(String email) {
 			this.email = email;
 		}
+
 		/**
 		 * @return the ageGroup
 		 */
 		public String getAgeGroup() {
 			return ageGroup;
 		}
+
 		/**
-		 * @param ageGroup the ageGroup to set
+		 * @param ageGroup
+		 *            the ageGroup to set
 		 */
 		public void setAgeGroup(String ageGroup) {
 			this.ageGroup = ageGroup;
 		}
+
 		/**
 		 * @return the trmtDateFrom
 		 */
 		public Date getTrmtDateFrom() {
 			return trmtDateFrom;
 		}
+
 		/**
-		 * @param trmtDateFrom the trmtDateFrom to set
+		 * @param trmtDateFrom
+		 *            the trmtDateFrom to set
 		 */
 		public void setTrmtDateFrom(Date trmtDateFrom) {
 			this.trmtDateFrom = trmtDateFrom;
 		}
+
 		/**
 		 * @return the trmtDateTo
 		 */
 		public Date getTrmtDateTo() {
 			return trmtDateTo;
 		}
+
 		/**
-		 * @param trmtDateTo the trmtDateTo to set
+		 * @param trmtDateTo
+		 *            the trmtDateTo to set
 		 */
 		public void setTrmtDateTo(Date trmtDateTo) {
 			this.trmtDateTo = trmtDateTo;
 		}
-		
-				
+
 	}
-	
+
 }
--- trunk/body-fs/src/com/bodyfs/ui/LoginWindow.java
+++ trunk/body-fs/src/com/bodyfs/ui/LoginWindow.java
@@ -1,10 +1,7 @@
 package com.bodyfs.ui;
 
-import javax.servlet.http.HttpSession;
-
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zkplus.databind.AnnotateDataBinder;
 import org.zkoss.zkplus.spring.SpringUtil;
@@ -14,70 +11,64 @@ import org.zkoss.zul.Textbox;
 import org.zkoss.zul.api.Window;
 
 import com.bodyfs.dao.ILoginDAO;
-import com.bodyfs.dao.impl.LoginDAO;
 import com.bodyfs.model.LoginInfo;
 
-public class LoginWindow extends GenericForwardComposer{
-	
-	
+public class LoginWindow extends GenericForwardComposer {
+
 	AnnotateDataBinder binder;
 
 	Textbox usertb;
 	Textbox pwdtb;
 	Button login;
-	
-	ILoginDAO dao = (ILoginDAO)SpringUtil.getBean("loginDAO");
-	
+
 	Window win;
-	
+
 	Label msg;
-	
+
 	private static final long serialVersionUID = 4311810570038057744L;
 
 	public void doAfterCompose(final Component comp) throws Exception {
 		super.doAfterCompose(comp);
-		win = (Window)comp;
-		//win.
+		win = (Window) comp;
+		// win.
 		binder = new AnnotateDataBinder(comp);
 		binder.loadAll();
 	}
-	
-	
-	public void doLogin(){
+
+	public void doLogin() {
 		String user = usertb.getValue();
 		String pwd = pwdtb.getValue();
-		
-		if((user!=null && user.isEmpty()) || (pwd!=null && pwd.isEmpty())){
+
+		if ((user != null && user.isEmpty()) || (pwd != null && pwd.isEmpty())) {
 			msg.setValue("*Need user name and password!");
 			return;
 		}
-		
-		LoginInfo userDetails = getLoginDetails(user, pwd); 
-		
-		if(userDetails==null){
+
+		LoginInfo userDetails = getLoginDetails(user, pwd);
+
+		if (userDetails == null) {
 			msg.setValue("*Wrong username or password!");
 			return;
 		}
-		
-		
-		session.setAttribute("LOGIN_CREDENTIALS",userDetails);
-		
+
+		session.setAttribute("LOGIN_CREDENTIALS", userDetails);
+
 		msg.setValue("");
 		Executions.sendRedirect("/index.zul");
 		binder.loadAll();
 	}
-	
-	public void onClick$login() throws Exception{
+
+	public void onClick$login() throws Exception {
 		doLogin();
 	}
-	
-	public void onOK(){
+
+	public void onOK() {
 		doLogin();
 	}
-	
-	
-	public LoginInfo getLoginDetails(String loginId,String password ){
-		LoginInfo login =  dao.verifyLoginDetails(loginId, password);
+
+	public LoginInfo getLoginDetails(String loginId, String password) {
+		final ILoginDAO dao = (ILoginDAO) SpringUtil.getBean("loginDAO");
+		LoginInfo login = dao.verifyLoginDetails(loginId, password);
 		return login;
 	}
 }

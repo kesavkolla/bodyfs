7ba283c22e67697d634bb42bf12ae57ab7ae83ed
amitagrawal84 <amitagrawal84@1fa41b22-b2bc-11de-a03c-433460d76767>
2009-12-07 19:08:44 +0000

    pagenation

--- trunk/body-fs/src/com/bodyfs/ui/WeeklyVisitComposer.java
+++ trunk/body-fs/src/com/bodyfs/ui/WeeklyVisitComposer.java
@@ -6,6 +6,8 @@ package com.bodyfs.ui;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.ForwardEvent;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
@@ -31,14 +33,15 @@ public class WeeklyVisitComposer extends GenericForwardComposer {
 
 	@Override
 	public void doAfterCompose(final Component comp) throws Exception {
-		super.doAfterCompose(comp);
 		personDAO = (IPersonDAO) SpringUtil.getBean("personDAO");
-		if (sessionScope.get("CURRENT_PATIENT_ID") != null) {
-			person = personDAO.getPerson((Long) sessionScope.get("CURRENT_PATIENT_ID"));
+		if (Executions.getCurrent().getAttribute("id")!= null) {//("CURRENT_PATIENT_ID") 
+			person = personDAO.getPerson((Long) Executions.getCurrent().getAttribute("id"));
 		} else {
 			person = personDAO.getByEmail("kesavkolla+bodyfs@gmail.com");
 		}
 		this.patientVisit = new PatientVisit();
+		super.doAfterCompose(comp);
+		
 	}
 
 	public void onSave(final ForwardEvent event) {
@@ -47,7 +50,10 @@ public class WeeklyVisitComposer extends GenericForwardComposer {
 			personDAO.createPatientVisit(patientVisit);
 		}
 	}
-
+	public int visitsCount(){
+		
+		return 3;
+	}
 	public void onNoteAdd(final Event event) {
 
 		LOGGER.error(event.getData());

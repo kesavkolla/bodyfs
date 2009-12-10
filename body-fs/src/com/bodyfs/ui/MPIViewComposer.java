6eae7b5f33cad0df6ca18ee11d62a190f2d5f539
kesavkolla <kesavkolla@1fa41b22-b2bc-11de-a03c-433460d76767>
2009-12-10 07:34:21 +0000

    Incomplete - task 2: Creating MPI charts","Type-Task, Priority-Medium http://code.google.com/p/bodyfs/issues/detail?id=2

--- body-fs/src/com/bodyfs/ui/MPIViewComposer.java
+++ body-fs/src/com/bodyfs/ui/MPIViewComposer.java
@@ -2,24 +2,26 @@
 package com.bodyfs.ui;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.zkoss.json.JSONArray;
 import org.zkoss.json.JSONObject;
-import org.zkoss.json.JSONValue;
-import org.zkoss.zk.au.out.AuInvoke;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Execution;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.ForwardEvent;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zkplus.spring.SpringUtil;
+import org.zkoss.zul.Checkbox;
 import org.zkoss.zul.api.Window;
 
 import com.bodyfs.dao.IMPIDao;
@@ -40,27 +42,69 @@ public class MPIViewComposer extends GenericForwardComposer {
 		return sdf.format(date);
 	}
 
+	/**
+	 * This method converts the given MPI object to JSON object
+	 * 
+	 * @param mpiData
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public JSONObject convertMPI(final MPIData mpiData) {
+		final JSONObject jsonObj = new JSONObject();
+		jsonObj.put("personId", mpiData.getPersonId());
+		jsonObj.put("examDate", mpiData.getExamDate().getTime());
+		jsonObj.put("LU1", mpiData.getLU1());
+		jsonObj.put("LU2", mpiData.getLU2());
+		jsonObj.put("p1", mpiData.getP1());
+		jsonObj.put("p2", mpiData.getP2());
+		jsonObj.put("HT1", mpiData.getHT1());
+		jsonObj.put("HT2", mpiData.getHT2());
+		jsonObj.put("SI1", mpiData.getSI1());
+		jsonObj.put("SI2", mpiData.getSI2());
+		jsonObj.put("TH1", mpiData.getTH1());
+		jsonObj.put("TH2", mpiData.getTH2());
+		jsonObj.put("LI1", mpiData.getLI1());
+		jsonObj.put("LI2", mpiData.getLI2());
+		jsonObj.put("SP1", mpiData.getSP1());
+		jsonObj.put("SP2", mpiData.getSP2());
+		jsonObj.put("LV1", mpiData.getLV1());
+		jsonObj.put("LV2", mpiData.getLV2());
+		jsonObj.put("KI1", mpiData.getKI1());
+		jsonObj.put("KI2", mpiData.getKI2());
+		jsonObj.put("BL1", mpiData.getBL1());
+		jsonObj.put("BL2", mpiData.getBL2());
+		jsonObj.put("GB1", mpiData.getGB1());
+		jsonObj.put("GB2", mpiData.getGB2());
+		jsonObj.put("ST1", mpiData.getST1());
+		jsonObj.put("ST2", mpiData.getST2());
+		jsonObj.put("high", mpiData.getHigh());
+		jsonObj.put("low", mpiData.getLow());
+		jsonObj.put("average", mpiData.getAverage());
+		return jsonObj;
+	}
+
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		final IMPIDao mpiDao = (IMPIDao) SpringUtil.getBean("MPIDao");
-		System.out.println("Id: " + Executions.getCurrent().getParameter("id"));
 		if (Executions.getCurrent().getParameterValues("id") != null) {
 			final MPIData mpi = mpiDao.getDataByDate(Long
 					.parseLong(Executions.getCurrent().getParameterValues("id")[0]), null);
-			System.out.println("MPI: " + mpi);
 			if (mpi != null) {
-				comp.setAttribute("mpi", mpi);
 				comp.getPage().setAttribute("mpi", mpi);
+				comp.getPage().setAttribute("mpidata", convertMPI(mpi));
 			}
 		}
 		super.doAfterCompose(comp);
 	}
 
+	/**
+	 * 
+	 * @return Returns the array of mpi dates
+	 */
 	public Date[] getExamDates() {
 		final IMPIDao mpiDao = (IMPIDao) SpringUtil.getBean("MPIDao");
 		final Collection<Date> dates = mpiDao.getExamDates(Long.parseLong(Executions.getCurrent().getParameterValues(
 				"id")[0]));
-		System.out.println(dates);
 		if (dates == null || dates.size() <= 0) {
 			return new Date[0];
 		}
@@ -101,53 +145,77 @@ public class MPIViewComposer extends GenericForwardComposer {
 		LOGGER.debug("Created MPI Data with Id: " + mpi.getId());
 	}
 
-	@SuppressWarnings("unchecked")
-	public void onKesav(final Event event) {
-		final Execution execution = Executions.getCurrent();
-		if (execution.getParameter("data_0") == null) {
-			return;
-		}
-		final JSONObject inpData = (JSONObject) JSONValue.parse(execution.getParameter("data_0"));
-		if (!inpData.containsKey("patid")) {
-			return;
-		}
-		if (!inpData.containsKey("examDate")) {
-			return;
-		}
+	/**
+	 * This method returns the mpi data that corresponds to the given patient id
+	 * and given date
+	 * 
+	 * @param patid
+	 * @param date
+	 * @return
+	 */
+	public String getMPIData(final long patid, final Date date) {
 		final IMPIDao mpiDao = (IMPIDao) SpringUtil.getBean("MPIDao");
-		final MPIData mpiData = mpiDao.getDataByDate(new Long((Integer) inpData.get("patid")), new Date((Long) inpData
-				.get("examDate")));
+		final MPIData mpiData = mpiDao.getDataByDate(patid, date);
 		if (mpiData == null) {
+			return "";
+		}
+		return convertMPI(mpiData).toJSONString();
+	}
+
+	/**
+	 * 
+	 * @param comp
+	 */
+	public void drawChart(Component comp) {
+		final Date date = (Date) comp.getAttribute("date");
+		final long patid = Long.parseLong(comp.getAttribute("patid").toString());
+		final String data = this.getMPIData(patid, date);
+		if (data.length() > 0) {
+			Clients.evalJavaScript("DrawChart(" + data + ")");
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void compareCharts(final org.zkoss.zul.api.Toolbar dateBar) {
+		final List<Date> dates = new ArrayList<Date>();
+		// Get all the child components of the toolbar
+		for (final Component comp : (List<Component>) dateBar.getChildren()) {
+			// skip if it's not the checkbox
+			if (!(comp instanceof Checkbox)) {
+				continue;
+			}
+			// skip if the checkbox is not selected
+			final Checkbox chkbox = (Checkbox) comp;
+			if (!chkbox.isChecked()) {
+				continue;
+			}
+			// checkbox name has the long time convert it to date
+			dates.add(new Date(Long.parseLong(chkbox.getName())));
+		}
+		if (dates.size() < 2) {
+			alert("Select at least 2 dates to do chart comparison");
 			return;
 		}
-		JSONObject jsonObj = new JSONObject();
-		jsonObj.put("personId", mpiData.getPersonId());
-		jsonObj.put("examDate", mpiData.getExamDate().getTime());
-		jsonObj.put("LU1", mpiData.getLU1());
-		jsonObj.put("LU2", mpiData.getLU2());
-		jsonObj.put("p1", mpiData.getP1());
-		jsonObj.put("p2", mpiData.getP2());
-		jsonObj.put("HT1", mpiData.getHT1());
-		jsonObj.put("HT2", mpiData.getHT2());
-		jsonObj.put("SI1", mpiData.getSI1());
-		jsonObj.put("SI2", mpiData.getSI2());
-		jsonObj.put("TH1", mpiData.getTH1());
-		jsonObj.put("TH2", mpiData.getTH2());
-		jsonObj.put("LI1", mpiData.getLI1());
-		jsonObj.put("LI2", mpiData.getLI2());
-		jsonObj.put("SP1", mpiData.getSP1());
-		jsonObj.put("SP2", mpiData.getSP2());
-		jsonObj.put("LV1", mpiData.getLV1());
-		jsonObj.put("LV2", mpiData.getLV2());
-		jsonObj.put("KI1", mpiData.getKI1());
-		jsonObj.put("KI2", mpiData.getKI2());
-		jsonObj.put("BL1", mpiData.getBL1());
-		jsonObj.put("BL2", mpiData.getBL2());
-		jsonObj.put("GB1", mpiData.getGB1());
-		jsonObj.put("GB2", mpiData.getGB2());
-		jsonObj.put("ST1", mpiData.getST1());
-		jsonObj.put("ST2", mpiData.getST2());
-
-		execution.addAuResponse("chartresp", new AuInvoke(event.getTarget(), "redrawChart", jsonObj));
+		if (dates.size() > 3) {
+			alert("Can not compare more than 3 charts");
+		}
+		// Get the patietn id from the toolbar's custom attribute
+		final Long patid = Long.parseLong(dateBar.getAttribute("patid").toString());
+		final JSONArray arr = new JSONArray();
+		// Get the MPIData for all the selected dates
+		final IMPIDao mpiDao = (IMPIDao) SpringUtil.getBean("MPIDao");
+		for (final Date date : dates) {
+			final MPIData mpiData = mpiDao.getDataByDate(patid, date);
+			if (mpiData != null) {
+				final JSONObject obj = convertMPI(mpiData);
+				obj.put("examDateF", sdf.format(mpiData.getExamDate()));
+				arr.add(obj);
+			}
+		}
+		if (arr.size() <= 0) {
+			alert("Error occurred in retrieving MPI data");
+			return;
+		}
+		Clients.evalJavaScript("DrawCompare(" + arr.toJSONString() + ")");
 	}
 }

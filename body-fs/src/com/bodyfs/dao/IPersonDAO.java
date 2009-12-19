a1b76d26b428925249ab410b5ccbebb820bdde55
amitagrawal84 <amitagrawal84@1fa41b22-b2bc-11de-a03c-433460d76767>
2009-12-19 17:48:18 +0000

    NPI-Complete funtionality with issues

--- body-fs/src/com/bodyfs/dao/IPersonDAO.java
+++ body-fs/src/com/bodyfs/dao/IPersonDAO.java
@@ -2,9 +2,23 @@ package com.bodyfs.dao;
 
 import java.util.Collection;
 
+import com.bodyfs.model.Cardiovascular;
+import com.bodyfs.model.Diet;
+import com.bodyfs.model.ENT;
+import com.bodyfs.model.FamilyMedHistory;
+import com.bodyfs.model.Gastrointestinal;
 import com.bodyfs.model.GeneralInfo;
+import com.bodyfs.model.GeneralSymptoms;
+import com.bodyfs.model.Genitourinary;
+import com.bodyfs.model.Gynecology;
+import com.bodyfs.model.Lifestyle;
+import com.bodyfs.model.Musculoskeletal;
+import com.bodyfs.model.Neuropsychological;
+import com.bodyfs.model.PastMedicalHistory;
 import com.bodyfs.model.PatientVisit;
 import com.bodyfs.model.Person;
+import com.bodyfs.model.Respiratory;
+import com.bodyfs.model.SkinHair;
 
 public interface IPersonDAO {
 	public Person getPerson(Long id);
@@ -28,4 +42,32 @@ public interface IPersonDAO {
 	public Collection<PatientVisit> GetPatientVisits(final Long personId);
 
 	public int countPatientVisits(final Long patid);
+	
+	public void createFamilyMedicalHistory(final FamilyMedHistory fmh);
+	
+	public void createPastMedicalHistory(final PastMedicalHistory pmh);
+	
+	public void createPatientDiet(final Diet yd);
+	
+	public void createLifeStyle(final Lifestyle yls);
+	
+	public void createGeneralSymptoms(final GeneralSymptoms gs);
+	
+	public void createENT(final ENT ent);
+	
+	public void createRespiratory(final Respiratory rp);	
+	
+	public void createCardiovascular(final Cardiovascular cv);
+	
+	public void createGastrointestinal(final Gastrointestinal gi);
+	
+	public void createMusculoskeletal(final Musculoskeletal ms);
+	
+	public void createSkinHair(final SkinHair sh);
+	
+	public void createNeuropsychological(final Neuropsychological np);
+	
+	public void createGenitourinary(final Genitourinary gen);
+	
+	public void createGynecology(final Gynecology gy);
 }
